beb2b1c8573e4e8a733ed32f8b31ddd916218d4f
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-04-04 09:19:22 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, make_response
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from models import db, Zookeeper, Enclosure, Animal
@@ -19,16 +20,41 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    response_body = f""
+    response_body += f"<ul>ID: {animal.id}</ul>"
+    response_body += f"<ul>Name: {animal.name} </ul>"
+    response_body += f"<ul>Species: {animal.species} </ul>"
+    response_body += f"<ul>Zookeeper: {animal.zookeeper.name} </ul>"
+    response_body += f"<ul>Enclosure: {animal.enclosure.environment}</ul>"
+
+    return make_response(response_body)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    response_body = f""
+    response_body += f"<ul>Id: {zookeeper.id} </ul>"
+    response_body += f"<ul>Name: {zookeeper.name} </ul>"
+    response_body += f"<ul>Birthday: {zookeeper.birthday} </ul>"
+
+    for animal in zookeeper.animals:
+        response_body += f"<ul>Animal: {animal.name} </ul>"
+
+    return make_response(response_body)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    response_body = f""
+    response_body += f"<ul>Id: {enclosure.id} </ul>"
+    response_body += f"<ul>Environment: {enclosure.environment} </ul>"
+    response_body += f"<ul>Open to Visitors: {enclosure.open_to_visitors} </ul>"
 
+    for animal in enclosure.animals:
+        response_body += f"<ul>Animal: {animal.name}</ul>"
+    
+    return make_response(response_body)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
